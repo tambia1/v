98097f042c1a0b3fc68d444b2b64b92e8585c2a8
a <a@a.com>
2023-10-02 00:21:05 +0300

    Settings

--- src/pages/pageHome/components/theme/Theme.tsx
+++ src/pages/pageHome/components/theme/Theme.tsx
@@ -2,14 +2,26 @@ import * as S from "./Theme.styles";
 import { List } from "@src/components/list/List";
 import { Text } from "@src/components/text/Text";
 import { Icon } from "@src/components/icon/Icon";
+import { useTheme } from "@src/components/theme/hooks/UseTheme";
+import { themes } from "@src/themes/Theme.types";
 
 export const Theme = () => {
+	const { theme, setTheme } = useTheme();
+
+	const handleOnClickLight = () => {
+		setTheme(themes.light);
+	};
+
+	const handleOnClickDark = () => {
+		setTheme(themes.dark);
+	};
+
 	return (
 		<S.Theme>
 			<List.Title>Theme</List.Title>
 
 			<List>
-				<List.Cell>
+				<List.Cell onClick={handleOnClickLight}>
 					<List.Cell.Image>
 						<Icon iconName="globe" />
 					</List.Cell.Image>
@@ -17,11 +29,11 @@ export const Theme = () => {
 						<Text>Light</Text>
 					</List.Cell.Text>
 					<List.Cell.Arrow>
-						<Icon iconName="v" />
+						<Icon iconName={theme.themeName === "light" ? "v" : ""} />
 					</List.Cell.Arrow>
 				</List.Cell>
 
-				<List.Cell>
+				<List.Cell onClick={handleOnClickDark}>
 					<List.Cell.Image>
 						<Icon iconName="globe" />
 					</List.Cell.Image>
@@ -29,7 +41,7 @@ export const Theme = () => {
 						<Text>Dark</Text>
 					</List.Cell.Text>
 					<List.Cell.Arrow>
-						<Icon iconName="" />
+						<Icon iconName={theme.themeName === "dark" ? "v" : ""} />
 					</List.Cell.Arrow>
 				</List.Cell>
 			</List>
