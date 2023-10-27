0d4ccc3e2a6e7f0ddbe1163a7261be276e569341
a <a@a.com>
2023-10-27 03:26:15 +0300

    Improve lang

--- src/pages/pageHome/components/applications/components/settings/components/language/Language.tsx
+++ src/pages/pageHome/components/applications/components/settings/components/language/Language.tsx
@@ -4,24 +4,17 @@ import { Icon } from "@src/components/icon/Icon";
 import { Flag } from "@src/components/flag/Flag";
 import { useLanguage } from "@src/language/hooks/UseLanguage";
 import { languages } from "@src/language/Language.types";
-import { Button } from "@src/components/button/Button";
-import { useState } from "react";
 import { Lang } from "@src/language/components/lang/Lang";
 
 export const Language = () => {
 	const { lang, language, setLanguage } = useLanguage();
-	const [selectedLanguage, setSelectedLanguage] = useState(language);
-
-	const handleOnClickSave = () => {
-		setLanguage(languages[selectedLanguage.languageName]);
-	};
 
 	const handleOnClickEnglish = () => {
-		setSelectedLanguage(languages.en);
+		setLanguage(languages.en);
 	};
 
 	const handleOnClickFinnish = () => {
-		setSelectedLanguage(languages.fi);
+		setLanguage(languages.fi);
 	};
 
 	return (
@@ -39,7 +32,7 @@ export const Language = () => {
 						<Lang>{lang.settings.language.english}</Lang>
 					</List.Cell.Text>
 					<List.Cell.Arrow>
-						<Icon iconName={selectedLanguage.languageName === "en" ? "v" : ""} />
+						<Icon iconName={language.languageName === "en" ? "v" : ""} />
 					</List.Cell.Arrow>
 				</List.Cell>
 
@@ -51,16 +44,10 @@ export const Language = () => {
 						<Lang>{lang.settings.language.finnish}</Lang>
 					</List.Cell.Text>
 					<List.Cell.Arrow>
-						<Icon iconName={selectedLanguage.languageName === "fi" ? "v" : ""} />
+						<Icon iconName={language.languageName === "fi" ? "v" : ""} />
 					</List.Cell.Arrow>
 				</List.Cell>
 			</List>
-
-			<List.Section>
-				<Button onClick={handleOnClickSave}>
-					<Lang>{lang.settings.language.save}</Lang>
-				</Button>
-			</List.Section>
 		</S.Language>
 	);
 };
