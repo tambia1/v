ecc46f8fdca28e35628ada11a680eaca5936c3d7
a <a@a.com>
2023-11-19 02:40:18 +0200

    Build 0.0.31

--- src/pages/pageHome/apps/camera/Camera.tsx
+++ src/pages/pageHome/apps/camera/Camera.tsx
@@ -9,7 +9,7 @@ export const Camera = () => {
 
 	const handleTakePicture = async () => {
 		try {
-			const stream = await navigator.mediaDevices.getUserMedia({ video: false });
+			const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 			if (videoRef.current) {
 				videoRef.current.srcObject = stream;
 			}
