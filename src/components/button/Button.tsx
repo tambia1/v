55f641880f115add840d6cd0aa412bfdbf0a9ba4
a <a@a.com>
2024-02-05 19:39:07 +0200

    Clean code

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import * as S from "./Button.styles";
 
-interface Props {
-	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
-	className?: string;
-	disabled?: boolean;
-	children?: React.ReactNode;
-}
+interface Props extends React.ComponentPropsWithoutRef<"button"> {}
 
 export const Button = (props: Props) => {
 	return (
