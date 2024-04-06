d9978b7940e17bb3f6e02de3adf0125d57bf0a61
a <a@a.com>
2024-04-07 01:01:26 +0300

    Chat

--- apps/app/src/pages/pageHome/components/desktop/apps/chat/components/name/components/page/Page.tsx
+++ apps/app/src/pages/pageHome/components/desktop/apps/chat/components/name/components/page/Page.tsx
@@ -17,7 +17,7 @@ interface Props {
 export const Page = ({ name }: Props) => {
 	const navigator = useNavigator();
 
-	const { sendMessage } = useWebSocket({ url: "ws://localhost:8080", onMessage: (message) => handleOnMessage(message) });
+	const { sendMessage } = useWebSocket({ url: "ws://[2a06:c701:9c57:5300:baca:5f04:b147:2869]:8080", onMessage: (message) => handleOnMessage(message) });
 
 	const [client, setClient] = useState<IClient>({ clientId: "", clientName: "" });
 	const [clients, setClients] = useState<IClient[]>([]);
