79c5a46ccb86eb9ba449936b0e0a90d7f4711652
a <a@a.com>
2024-04-28 01:53:20 +0300

    GraphQL

--- app/src/pages/pageHome/components/desktop/apps/testGraphQl/GraphQl.styles.tsx
+++ app/src/pages/pageHome/components/desktop/apps/testGraphQl/GraphQl.styles.tsx
@@ -7,6 +7,7 @@ export const GraphQl = styled.div`
 	display: flex;
 	flex-direction: column;
 
+	padding: 1rem;
 	box-sizing: border-box;
 
 	background-color: ${(props) => props.theme.color.normalBgSelected};
