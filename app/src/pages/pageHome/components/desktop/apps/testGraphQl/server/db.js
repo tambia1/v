09b4a2dfa31c0cf05b6e538c0b65a66ad5ad5047
a <a@a.com>
2024-04-27 21:19:31 +0300

    GraphQL server

---
+++ app/src/pages/pageHome/components/desktop/apps/testGraphQl/server/db.js
@@ -0,0 +1,25 @@
+let games = [
+	{ id: "1", title: "Zelda", platform: ["Switch"] },
+	{ id: "2", title: "Final Fantasy 7", platform: ["PS5", "Xbox"] },
+	{ id: "3", title: "Elden Ring", platform: ["PS5", "Xbox", "PC"] },
+	{ id: "4", title: "Mario", platform: ["Switch"] },
+	{ id: "5", title: "Pokimon", platform: ["PS5", "Xbox", "PC"] },
+];
+
+let authors = [
+	{ id: "1", name: "mario" },
+	{ id: "2", name: "yoshi" },
+	{ id: "3", name: "peach" },
+];
+
+let reviews = [
+	{ id: "1", rating: 9, content: "some content", authorId: "1", gameId: "2" },
+	{ id: "2", rating: 10, content: "some content", authorId: "2", gameId: "1" },
+	{ id: "3", rating: 7, content: "some content", authorId: "3", gameId: "3" },
+	{ id: "4", rating: 5, content: "some content", authorId: "2", gameId: "4" },
+	{ id: "5", rating: 8, content: "some content", authorId: "2", gameId: "5" },
+	{ id: "6", rating: 7, content: "some content", authorId: "1", gameId: "2" },
+	{ id: "7", rating: 10, content: "some content", authorId: "3", gameId: "1" },
+];
+
+export default { games, authors, reviews };
---
+++ app/src/pages/pageHome/components/desktop/apps/testGraphQl/server/index.js
@@ -0,0 +1,84 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./db.js";
+
+const resolvers = {
+	Query: {
+		games() {
+			return db.games;
+		},
+		game(_parent, args) {
+			return db.games.find((game) => game.id === args.id);
+		},
+		reviews() {
+			return db.reviews;
+		},
+		review(_parent, args) {
+			return db.reviews.find((review) => review.id === args.id);
+		},
+		authors() {
+			return db.authors;
+		},
+		author(_parent, args) {
+			return db.authors.find((author) => author.id === args.id);
+		},
+	},
+	Game: {
+		reviews(parent) {
+			return db.reviews.filter((r) => r.gameId === parent.id);
+		},
+	},
+	Author: {
+		reviews(parent) {
+			return db.reviews.filter((r) => r.authorId === parent.id);
+		},
+	},
+	Review: {
+		author(parent) {
+			return db.authors.find((a) => a.id === parent.authorId);
+		},
+		game(parent) {
+			return db.authors.find((g) => g.id === parent.gameId);
+		},
+	},
+	Mutation: {
+		deleteGame(_parent, args) {
+			db.games = db.games.filter((g) => g.id !== args.id);
+
+			return db.games;
+		},
+		addGame(_parent, args) {
+			const game = {
+				...args.game,
+				id: ~~(Math.random() * 1000),
+			};
+
+			db.games.push(game);
+
+			return game;
+		},
+		updateGame(_parent, args) {
+			db.games = db.games.map((g) => {
+				if (g.id === args.id) {
+					return { ...g, ...args.edits };
+				}
+
+				return g;
+			});
+
+			return db.games.find((g) => g.id === args.id);
+		},
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+	listen: { port: 4000 },
+});
+
+console.log("GraphQL server ready at port 4000");
