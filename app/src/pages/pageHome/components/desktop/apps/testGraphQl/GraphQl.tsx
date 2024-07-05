c586fe913f583bd587c8b43f3c7376d080886ebd
a <a@a.com>
2024-07-05 17:48:35 +0300

    Remove graphQLReact Query

--- app/src/pages/pageHome/components/desktop/apps/testGraphQl/GraphQl.tsx
+++ app/src/pages/pageHome/components/desktop/apps/testGraphQl/GraphQl.tsx
@@ -1,13 +1,11 @@
 import * as S from "./GraphQl.styles";
 import { GraphQlApolloClient } from "./components/graphQlApolloClient/GraphQlApolloClient";
-import { GraphQlReactQuery } from "./components/graphQlReactQuery/GraphQlReactQuery";
 
 export const TestGraphQl = () => {
 	return (
 		<S.TestGraphQl>
 			<S.Container>
 				<GraphQlApolloClient />
-				<GraphQlReactQuery />
 			</S.Container>
 		</S.TestGraphQl>
 	);
