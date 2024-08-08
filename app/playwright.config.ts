3d43d843caffb2423f76197279a9a0afb0ffd2fe
a <a@a.com>
2024-08-09 01:51:03 +0300

    Playwright

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -75,4 +75,6 @@ export default defineConfig({
 	//   url: 'http://127.0.0.1:3000',
 	//   reuseExistingServer: !process.env.CI,
 	// },
+
+	snapshotPathTemplate: "{testDir}/screenshots/{testFilePath}/{arg}{ext}",
 });
