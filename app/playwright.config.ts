c9339d4e3c6771e543cc098bf0aa9a9c59d27f2b
a <a@a.com>
2024-08-11 00:17:27 +0300

    Playwright

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -29,6 +29,10 @@ export default defineConfig({
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
+		video: "on",
+		launchOptions: {
+			slowMo: 1000,
+		},
 	},
 
 	/* Configure projects for major browsers */
