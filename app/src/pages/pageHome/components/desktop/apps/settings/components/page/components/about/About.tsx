9fdec7f16942f4cc547b3b491e579d309c0d1827
a <a@a.com>
2024-09-14 21:04:35 +0300

    Device

--- app/src/pages/pageHome/components/desktop/apps/settings/components/page/components/about/About.tsx
+++ app/src/pages/pageHome/components/desktop/apps/settings/components/page/components/about/About.tsx
@@ -1,6 +1,7 @@
 import * as S from "./About.styles";
 import { Text } from "@src/components/text/Text";
 import { lang } from "@src/locales/i18n";
+import { Device } from "@src/utils/Device";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -34,14 +35,22 @@ export const About = () => {
 	return (
 		<S.About>
 			<Text size="l">{t(lang.settings.about.text)}</Text>
+
 			<S.Spacer />
 			<Text size="m">
 				{dimensions.width}x{dimensions.height}
 			</Text>
+
 			<S.Spacer />
 			<Text size="m">{dimensions.orientation}</Text>
+
 			<S.Spacer />
 			<Text size="m">{navigator.userAgent}</Text>
+			<Text size="m">{Device.getDeviceInfo().deviceName}</Text>
+			<Text size="m">{Device.getDeviceInfo().iosName}</Text>
+			<Text size="m">{Device.getDeviceInfo().iosVersion}</Text>
+			<Text size="m">{Device.getDeviceInfo().browserName}</Text>
+			<Text size="m">{Device.getDeviceInfo().browserVersion}</Text>
 		</S.About>
 	);
 };
--- app/src/utils/Device.ts
+++ app/src/utils/Device.ts
@@ -39,8 +39,80 @@ const preventDesktopRightClick = () => {
 	);
 };
 
+const getDeviceInfo = () => {
+	const ua = navigator.userAgent;
+
+	let deviceName = "Unknown Device";
+	let iosName = "";
+	let iosVersion = "";
+	let browserName = "Unknown Browser";
+	let browserVersion = "";
+
+	if (/iPad|iPhone|iPod/.test(ua)) {
+		iosName = "iOS";
+
+		const versionMatch = ua.match(/OS (\d+_\d+)/);
+		iosVersion = versionMatch ? versionMatch[1].replace("_", ".") : "Unknown";
+
+		if (/iPhone/.test(ua)) {
+			deviceName = "iPhone";
+		} else if (/iPad/.test(ua)) {
+			deviceName = "iPad";
+		} else if (/iPod/.test(ua)) {
+			deviceName = "iPod";
+		} else {
+			deviceName = "Apple Device";
+		}
+	} else if (/Android/.test(ua)) {
+		iosName = "Android";
+		deviceName = "Android Device";
+	} else if (/Windows/.test(ua)) {
+		deviceName = "Windows Device";
+	} else if (/Macintosh/.test(ua)) {
+		deviceName = "Mac Device";
+	}
+
+	if (/Chrome/.test(ua) && !/Edge/.test(ua)) {
+		browserName = "Chrome";
+		const versionMatch = ua.match(/Chrome\/(\d+\.\d+)/);
+		browserVersion = versionMatch ? versionMatch[1] : "Unknown";
+	} else if (/Firefox/.test(ua)) {
+		browserName = "Firefox";
+		const versionMatch = ua.match(/Firefox\/(\d+\.\d+)/);
+		browserVersion = versionMatch ? versionMatch[1] : "Unknown";
+	} else if (/Safari/.test(ua) && !/Chrome/.test(ua)) {
+		browserName = "Safari";
+		const versionMatch = ua.match(/Version\/(\d+\.\d+)/);
+		browserVersion = versionMatch ? versionMatch[1] : "Unknown";
+	} else if (/Edge/.test(ua)) {
+		browserName = "Edge";
+		const versionMatch = ua.match(/Edge\/(\d+\.\d+)/);
+		browserVersion = versionMatch ? versionMatch[1] : "Unknown";
+	} else if (/MSIE/.test(ua) || /Trident/.test(ua)) {
+		browserName = "Internet Explorer";
+		const versionMatch = ua.match(/(MSIE \d+.\d+|rv:\d+.\d+)/);
+		browserVersion = versionMatch ? versionMatch[1].replace(/MSIE |rv:/, "") : "Unknown";
+	}
+
+	const isMobile = /Mobi/.test(ua);
+	const isTablet = /Tablet/.test(ua) || (/iPad/.test(ua) && !/Mobile/.test(ua));
+	const isDesktop = !isMobile && !isTablet;
+
+	return {
+		deviceName,
+		iosName,
+		iosVersion,
+		browserName,
+		browserVersion,
+		isMobile,
+		isTablet,
+		isDesktop,
+	};
+};
+
 export const Device = {
 	preventIosDoubleClickToZoom,
 	preventDesktopContextMenu,
 	preventDesktopRightClick,
+	getDeviceInfo,
 };
