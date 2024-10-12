1c70c6ecb51370097025b5ffdf713f8ca5407fba
a <a@a.com>
2024-10-12 23:19:58 +0300

    UseTimeout

--- app/src/hooks/UseTimeout.tsx
+++ app/src/hooks/UseTimeout.tsx
@@ -1,48 +1,25 @@
 import { useEffect, useRef } from "react";
 
-export const useTimeout = (
-	callbackTimeout: () => void,
-	timeout: number,
-	callbackInterval: () => void,
-	interval: number
-) => {
+export const useTimeout = (callbackTimeout: () => void, timeout: number) => {
 	const callbackTimeoutRef = useRef(callbackTimeout);
-	const callbackIntervalRef = useRef(callbackInterval);
 
 	useEffect(() => {
 		callbackTimeoutRef.current = callbackTimeout;
 	}, [callbackTimeout]);
 
 	useEffect(() => {
-		callbackIntervalRef.current = callbackInterval;
-	}, [callbackInterval]);
-
-	useEffect(() => {
 		if (timeout <= 0) {
 			return;
 		}
 
-		if (interval <= 0) {
-			interval = timeout;
-		}
-
 		const timeoutId = setTimeout(() => {
-			clearTimers();
+			clearTimeout(timeoutId);
 
 			callbackTimeoutRef.current();
 		}, timeout);
 
-		const intervalId = setInterval(() => {
-			callbackIntervalRef.current();
-		}, interval);
-
-		const clearTimers = () => {
-			clearTimeout(timeoutId);
-			clearInterval(intervalId);
-		};
-
 		return () => {
-			clearTimers();
+			clearTimeout(timeoutId);
 		};
-	}, [timeout, interval]);
+	}, [timeout]);
 };
