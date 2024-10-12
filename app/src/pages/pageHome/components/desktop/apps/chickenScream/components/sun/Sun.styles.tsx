e7b7bcfdd20a498ea2a7b3a72a43cb948e5e4592
a <a@a.com>
2024-10-12 04:15:02 +0300

    Chicken

--- app/src/pages/pageHome/components/desktop/apps/chickenScream/components/sun/Sun.styles.tsx
+++ app/src/pages/pageHome/components/desktop/apps/chickenScream/components/sun/Sun.styles.tsx
@@ -2,8 +2,8 @@ import styled, { keyframes } from "styled-components";
 
 const shadow = keyframes`
     0% { box-shadow: 0px 0px 40px 15px rgb(255, 235, 145); }
-    50% { box-shadow: 0px 0px 40px 25px rgb(255, 235, 45); }
-    100% { box-shadow: 0px 0px 40px 5px rgb(255, 235, 145); }
+    50% { box-shadow: 0px 10px 50px 25px rgb(255, 235, 145); }
+    100% { box-shadow: 10px 0px 40px 10px rgb(255, 235, 145); }
 `;
 
 export const Sun = styled.div`
