45557853febbadbc02b387b4f87991889b7b8257
a <a@a.com>
2024-10-19 16:34:08 +0300

    Redis

--- app/src/pages/pageHome/components/desktop/apps/redis/components/user/User.tsx
+++ app/src/pages/pageHome/components/desktop/apps/redis/components/user/User.tsx
@@ -15,11 +15,11 @@ export const User = () => {
 
 	const [inputData, setInputData] = useState({
 		email: {
-			value: "",
+			value: "a",
 			disabled: false,
 		},
 		password: {
-			value: "",
+			value: "a",
 			disabled: false,
 		},
 	});
