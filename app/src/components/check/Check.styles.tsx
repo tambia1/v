29e7a7e43014ea656446879efc911a82125208aa
a <a@a.com>
2024-10-19 16:27:13 +0300

    Check

--- app/src/components/check/Check.styles.tsx
+++ app/src/components/check/Check.styles.tsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
+import { Icon } from "../icon/Icon.styles";
 
 export const Check = styled.div`
+	width: 2.5rem;
+	height: 2.5rem;
 	display: flex;
+	overflow: hidden;
+
+	& ${Icon}{
+		width: 120%;
+    	height: 120%;
+    	margin: -10%;
+	}
 `;
 
 export const Dot = styled.div<{ $checkState: boolean }>`
