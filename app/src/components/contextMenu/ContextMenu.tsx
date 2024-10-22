6e550b9d9b98ba5a157c060b51ad650b2ee9e777
a <a@a.com>
2024-10-23 02:22:20 +0300

    ContextMenu

--- app/src/components/contextMenu/ContextMenu.tsx
+++ app/src/components/contextMenu/ContextMenu.tsx
@@ -3,27 +3,46 @@ import { List } from "../list/List";
 import * as S from "./ContextMenu.styles";
 import { Item } from "./components/item/Item";
 
+export type PositionVer = "top" | "middle" | "bottom";
+export type PositionHor = "left" | "center" | "right";
+
 export type Props = HTMLAttributes<HTMLDivElement> & {
 	className?: string;
 	children: ReactNode;
 	visible: boolean;
 	onClickCover?: () => void;
 	refButton: RefObject<HTMLElement>;
+	PositionVer?: PositionVer;
+	PositionHor?: PositionHor;
 };
 
-export const ContextMenu = ({ className, children, visible, onClickCover, refButton, ...rest }: Props) => {
+export const ContextMenu = ({ className, children, visible, onClickCover, refButton, PositionVer = "bottom", PositionHor = "right", ...rest }: Props) => {
 	const refCover = useRef<HTMLDivElement>(null);
+	const refItems = useRef<HTMLDivElement>(null);
 	const [position, setPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
 	const [isItemsOpen, setIsItemsOpen] = useState(false);
 
 	useLayoutEffect(() => {
-		if (refButton.current && refCover.current && visible) {
-			const coverRect = refCover.current.getBoundingClientRect();
-			const buttonRect = refButton.current.getBoundingClientRect();
+		if (refButton.current && refCover.current && refItems.current && visible) {
+			const rectCover = refCover.current.getBoundingClientRect();
+			const rectItems = refItems.current.getBoundingClientRect();
+			const rectButton = refButton.current.getBoundingClientRect();
+
+			const verMap: { [K in PositionVer]: number } = {
+				top: rectButton.top - rectItems.height,
+				middle: rectButton.top + rectButton.height / 2 - rectItems.height / 2,
+				bottom: rectButton.bottom,
+			};
+
+			const horMap: { [K in PositionHor]: number } = {
+				left: rectButton.left - rectItems.width,
+				center: rectButton.left + rectButton.width / 2 - rectItems.width / 2,
+				right: rectButton.right,
+			};
 
 			setPosition({
-				top: buttonRect.bottom - coverRect.top,
-				left: buttonRect.left - coverRect.left + refButton.current.clientWidth,
+				top: verMap[PositionVer] - rectCover.top,
+				left: horMap[PositionHor] - rectCover.left,
 			});
 
 			setTimeout(() => {
@@ -32,7 +51,7 @@ export const ContextMenu = ({ className, children, visible, onClickCover, refBut
 		} else {
 			setIsItemsOpen(false);
 		}
-	}, [refButton.current, visible]);
+	}, [refButton.current, visible, PositionVer, PositionHor]);
 
 	const handleOnClick = () => {
 		onClickCover?.();
@@ -40,7 +59,7 @@ export const ContextMenu = ({ className, children, visible, onClickCover, refBut
 
 	return (
 		<S.ContextMenu ref={refCover} className={className} onClick={handleOnClick} $visible={visible} {...rest}>
-			<S.Items $left={position.left} $top={position.top}>
+			<S.Items ref={refItems} $left={position.left} $top={position.top}>
 				<S.ItemsContainer $isOpen={isItemsOpen}>
 					<List>{children}</List>
 				</S.ItemsContainer>
