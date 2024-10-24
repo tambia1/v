a2c861c75803db7c1f058c97f4f0d8d8b878dc48
a <a@a.com>
2024-10-25 02:21:24 +0300

    PopupMenu

--- app/src/components/popupMenu/components/item/Item.tsx
+++ app/src/components/popupMenu/components/item/Item.tsx
@@ -1,9 +1,15 @@
 import type { HTMLAttributes, ReactNode } from "react";
+import * as S from "./Item.styles";
 
 export type Props = HTMLAttributes<HTMLDivElement> & {
+	className?: string;
 	children: ReactNode;
 };
 
-export const Item = ({ children }: Props) => {
-	return <>{children}</>;
+export const Item = ({ children, className, ...rest }: Props) => {
+	return (
+		<S.Item className={className} {...rest}>
+			{children}
+		</S.Item>
+	);
 };
