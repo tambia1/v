c5b81b690fd1e7e1e3f8cc29942a58bf37ff2e1c
a <a@a.com>
2024-10-29 00:47:55 +0200

    Redis

--- app/src/pages/pageHome/components/desktop/apps/redis/Redis.tsx
+++ app/src/pages/pageHome/components/desktop/apps/redis/Redis.tsx
@@ -1,7 +1,7 @@
 import * as S from "./Redis.styles";
-import { StoreUser } from "./components/user/stores/StoreUser";
-import { User } from "./components/user/User";
 import { Home } from "./components/home/Home";
+import { User } from "./components/user/User";
+import { StoreUser } from "./components/user/stores/StoreUser";
 
 export const Redis = () => {
 	const storeUser = StoreUser();
@@ -9,13 +9,9 @@ export const Redis = () => {
 
 	return (
 		<S.Redis>
-			<S.Transition $visible={!isLoggedin}>
-				<User />
-			</S.Transition>
+			<S.Transition $visible={!isLoggedin}>{!isLoggedin && <User />}</S.Transition>
 
-			<S.Transition $visible={isLoggedin}>
-				<Home />
-			</S.Transition>
+			<S.Transition $visible={isLoggedin}>{isLoggedin && <Home />}</S.Transition>
 		</S.Redis>
 	);
 };
