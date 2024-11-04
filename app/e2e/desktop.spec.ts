ddf36a3b1bccd576539b8b95a1a47c4c736c5b87
a <a@a.com>
2024-11-04 22:45:03 +0200

    Test

--- app/e2e/desktop.spec.ts
+++ app/e2e/desktop.spec.ts
@@ -3,9 +3,15 @@ import { expect, test } from "@playwright/test";
 test("Desktop", async ({ page }) => {
 	await page.goto("https://localhost:5000/v/");
 
-	const desktop = await page.getByText("Guest");
-	await expect(desktop).toBeVisible();
-	await expect(page).toHaveScreenshot("desktop_home.png");
+	const splash = await page.getByLabel("logo");
+	await expect(splash).toBeVisible();
+	await page.pause();
+	await expect(page).toHaveScreenshot("desktop_splash.png");
+
+	const home = await page.getByText("Guest");
+	await expect(home).toBeVisible();
+	await page.pause();
+	await expect(page).toHaveScreenshot("desktop_guest.png");
 
 	await page.getByText("User").click();
 	await page.getByPlaceholder("Email (a, b)").click();
@@ -13,18 +19,24 @@ test("Desktop", async ({ page }) => {
 	await page.getByPlaceholder("Password (a, b)").click();
 	await page.getByPlaceholder("Password (a, b)").fill("a");
 	await page.getByRole("button", { name: "Login" }).click();
-	const userName = await page.getByText("John", { exact: true });
+	const userName = await page.getByText("Welcome John Admin!", { exact: true });
 	await expect(userName).toBeVisible();
-	await expect(page).toHaveScreenshot("desktop_after_login.png");
+	await page.pause();
+	await expect(page).toHaveScreenshot("desktop_login.png");
+
+	await expect(userName).not.toBeVisible();
+	await page.pause();
+	await expect(page).toHaveScreenshot("desktop_user.png");
 
 	const appSettings = await page.getByText("Settings");
 	await expect(appSettings).toBeVisible();
 	await appSettings.click();
 	await expect(page.getByText("Appearance")).toBeVisible();
+	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_settings.png");
 
 	await page.locator("svg").filter({ hasText: "iconXCircle" }).click();
-	await page.waitForTimeout(1000);
 	await expect(userName).toBeVisible();
-	await expect(page).toHaveScreenshot("desktop_after_settings.png");
+	await page.pause();
+	await expect(page).toHaveScreenshot("desktop_home.png");
 });
