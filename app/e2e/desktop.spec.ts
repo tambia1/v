6cbb542f7c41a98aae2a962ab1dc8b9ecfcc37f5
a <a@a.com>
2024-11-04 22:47:43 +0200

    Tests

--- app/e2e/desktop.spec.ts
+++ app/e2e/desktop.spec.ts
@@ -5,12 +5,10 @@ test("Desktop", async ({ page }) => {
 
 	const splash = await page.getByLabel("logo");
 	await expect(splash).toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_splash.png");
 
 	const home = await page.getByText("Guest");
 	await expect(home).toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_guest.png");
 
 	await page.getByText("User").click();
@@ -21,22 +19,18 @@ test("Desktop", async ({ page }) => {
 	await page.getByRole("button", { name: "Login" }).click();
 	const userName = await page.getByText("Welcome John Admin!", { exact: true });
 	await expect(userName).toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_login.png");
 
 	await expect(userName).not.toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_user.png");
 
 	const appSettings = await page.getByText("Settings");
 	await expect(appSettings).toBeVisible();
 	await appSettings.click();
 	await expect(page.getByText("Appearance")).toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_settings.png");
 
 	await page.locator("svg").filter({ hasText: "iconXCircle" }).click();
 	await expect(userName).toBeVisible();
-	await page.pause();
 	await expect(page).toHaveScreenshot("desktop_home.png");
 });
