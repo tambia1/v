82d686e8083925f04ae7735bb4228402b0a663a6
a <a@a.com>
2024-11-08 04:58:32 +0200

    Visuals

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -11,8 +11,8 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: "./src/e2e",
-	outputDir: "./e2e-temp/e2e-results",
+	testDir: "./src/visuals",
+	outputDir: "./visuals-temp/results",
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,7 +22,7 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: [["html", { outputFolder: "./e2e-temp/e2e-report" }]],
+	reporter: [["html", { outputFolder: "./visuals-temp/report" }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
