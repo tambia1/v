5e9bc4fea35db55815fad0063eee0c5be0f0bb61
a <a@a.com>
2024-11-16 21:02:57 +0200

    Tests

--- app/src/pages/pageHome/components/desktop/apps/testEdit/TestEdit.test.tsx
+++ app/src/pages/pageHome/components/desktop/apps/testEdit/TestEdit.test.tsx
@@ -1,5 +1,5 @@
 import { Theme } from "@src/theme/Theme";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { TestEdit } from "./TestEdit";
 
 const renderComponent = () => {
