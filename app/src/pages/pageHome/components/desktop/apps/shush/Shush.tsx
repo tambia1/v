6a407d6ab2317f5a00ff70a02a3ffd84af47c609
a <a@a.com>
2025-03-06 02:10:35 +0200

    Build 1.3.81

--- app/src/pages/pageHome/components/desktop/apps/shush/Shush.tsx
+++ app/src/pages/pageHome/components/desktop/apps/shush/Shush.tsx
@@ -1,8 +1,8 @@
 import { Icon } from "@src/components/icon/Icon";
 import { useEffect, useRef, useState } from "react";
 import * as S from "./Shush.styles";
-import shush0 from "./assets/shush_0.mp3";
-import shush1 from "./assets/shush_1.mp3";
+import shush0 from "./assets/shush_0.aac";
+import shush1 from "./assets/shush_1.aac";
 import { useMicrophone } from "./hooks/useMicrophone";
 
 export const Shush = () => {
