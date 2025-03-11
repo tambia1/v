c910cba26bb787933721c6a7d2699d21bc1bb891
a <a@a.com>
2025-03-11 22:57:41 +0200

    Build 1.4.17

--- app/src/pages/pageHome/components/desktop/apps/shush/Shush.tsx
+++ app/src/pages/pageHome/components/desktop/apps/shush/Shush.tsx
@@ -1,40 +1,41 @@
 import { Icon } from "@src/components/icon/Icon";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as S from "./Shush.styles";
 import shush0 from "./assets/shush_0.mp3";
 import shush1 from "./assets/shush_1.mp3";
-import { useAudio } from "./hooks/useAudio";
-
-const audioList = [shush0, shush1];
+import { useMicrophone } from "./hooks/useMicrophone";
 
 export const Shush = () => {
-	const { mic, audio } = useAudio();
+	const { isListening, volume } = useMicrophone();
+	const audioRefs = useRef([new Audio(shush0), new Audio(shush1)]);
+	const [isPlaying, setIsPlaying] = useState(false);
 	const [maxVolume, setMaxVolume] = useState(0.2);
 
 	useEffect(() => {
-		mic.startMic();
-	}, [mic.startMic]);
+		if (volume >= maxVolume && !isPlaying) {
+			setIsPlaying(true);
 
-	useEffect(() => {
-		if (mic.volume >= maxVolume && !audio.isPlaying) {
-			const randomIndex = Math.floor(Math.random() * audioList.length);
-			const randomAudio = audioList[randomIndex];
+			const randomIndex = Math.floor(Math.random() * audioRefs.current.length);
+			const randomAudio = audioRefs.current[randomIndex];
 
-			audio.playAudio(randomAudio);
+			randomAudio.play();
+
+			randomAudio.onended = () => {
+				setIsPlaying(false);
+			};
 		}
-	}, [mic.volume, audio.isPlaying, audio.playAudio, maxVolume]);
+	}, [volume, isPlaying, maxVolume]);
 
 	return (
 		<S.Shush>
 			<S.Row>
-				{!mic.isListening && <Icon iconName="iconMicOff" stroke="red" />}
-				{mic.isListening && <Icon iconName="iconMic" stroke="green" />}
-
-				{!audio.isPlaying && <Icon iconName="iconVolumeX" stroke="gray" />}
-				{audio.isPlaying && <Icon iconName="iconVolume2" stroke="yellow" />}
+				{!isListening && <Icon iconName="iconMicOff" stroke="red" />}
+				{isListening && <Icon iconName="iconMic" stroke="green" />}
+				{!isPlaying && <Icon iconName="iconVolumeX" stroke="gray" />}
+				{isPlaying && <Icon iconName="iconVolume2" stroke="yellow" />}
 			</S.Row>
 
-			<S.ProgressStyled size="available" percent={mic.volume * 100} />
+			<S.ProgressStyled size="available" percent={volume * 100} />
 			<S.SliderStyled size="available" value={maxVolume} onChange={(value) => setMaxVolume(value)} />
 		</S.Shush>
 	);
