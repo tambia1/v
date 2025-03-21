35a0044758aee1803193f07a70007b579bc90762
a <a@a.com>
2025-03-21 18:29:58 +0200

    Clean code

--- app/src/pages/pageHome/components/desktop/apps/clashRoyale/pages/game/UtilsPath.ts
+++ app/src/pages/pageHome/components/desktop/apps/clashRoyale/pages/game/UtilsPath.ts
@@ -1,5 +1,5 @@
 const getDistance = (x1: number, y1: number, x2: number, y2: number) => {
-	return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 };
 
 const findClosestXYOfValue = (x1: number, y1: number, value: number, grid: number[][]) => {
@@ -9,7 +9,7 @@ const findClosestXYOfValue = (x1: number, y1: number, value: number, grid: numbe
 
 	for (let y = 0; y < grid.length; y++) {
 		for (let x = 0; x < grid[y].length; x++) {
-			if (grid[y][x] == value) {
+			if (grid[y][x] === value) {
 				const distance = getDistance(x1, y1, x, y);
 
 				if (distance < distanceMin) {
@@ -24,7 +24,7 @@ const findClosestXYOfValue = (x1: number, y1: number, value: number, grid: numbe
 	return { x: x2, y: y2 };
 };
 
-type IStack = {
+type Stack = {
 	[key: string]: {
 		x: number;
 		y: number;
@@ -41,27 +41,27 @@ const findPath = (x1: number, y1: number, x2: number, y2: number, grid: number[]
 		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 	};
 
-	const calcNeighbours = (x: number, y: number, stack: IStack) => {
+	const calcNeighbours = (x: number, y: number, stack: Stack) => {
 		for (let i = -1; i <= 1; i++) {
 			for (let j = -1; j <= 1; j++) {
-				if (stack[x + j + "," + (y + i)] == undefined && grid[y + i]?.[x + j] != undefined) {
+				if (stack[`${x + j},${y + i}`] === undefined && grid[y + i]?.[x + j] !== undefined) {
 					const d1 = calcDistance(x1, y1, x + j, y + i);
 					const d2 = calcDistance(x2, y2, x + j, y + i);
 
-					if (grid[y + i][x + j] == 0 || d2 == 0) {
-						stack[x + j + "," + (y + i)] = { x: x + j, y: y + i, d1, d2, v: false, px: x, py: y };
+					if (grid[y + i][x + j] === 0 || d2 === 0) {
+						stack[`${x + j},${y + i}`] = { x: x + j, y: y + i, d1, d2, v: false, px: x, py: y };
 					}
 				}
 			}
 		}
 	};
 
-	const getNextStack = (stack: IStack) => {
+	const getNextStack = (stack: Stack) => {
 		let nextStack = null;
 		let maxd2 = Number.MAX_VALUE;
 
 		for (const k in stack) {
-			if (stack[k].v == false && stack[k].d2 <= maxd2) {
+			if (stack[k].v === false && stack[k].d2 <= maxd2) {
 				nextStack = stack[k];
 				maxd2 = stack[k].d2;
 			}
@@ -70,7 +70,7 @@ const findPath = (x1: number, y1: number, x2: number, y2: number, grid: number[]
 		return nextStack;
 	};
 
-	const stack: IStack = {};
+	const stack: Stack = {};
 	calcNeighbours(x1, y1, stack);
 
 	let nextStack = null;
@@ -79,19 +79,19 @@ const findPath = (x1: number, y1: number, x2: number, y2: number, grid: number[]
 		nextStack = getNextStack(stack);
 
 		if (nextStack != null) {
-			stack[nextStack.x + "," + nextStack.y].v = true;
+			stack[`${nextStack.x},${nextStack.y}`].v = true;
 			calcNeighbours(nextStack.x || 0, nextStack.y || 0, stack);
 		}
-	} while ((nextStack?.x != x2 || nextStack?.y != y2) && nextStack != null);
+	} while ((nextStack?.x !== x2 || nextStack?.y !== y2) && nextStack != null);
 
 	const path = [];
 
 	do {
 		if (nextStack != null) {
 			path.unshift(nextStack);
-			nextStack = stack[nextStack.px + "," + nextStack.py];
+			nextStack = stack[`${nextStack.px},${nextStack.py}`];
 		}
-	} while ((nextStack?.x != x1 || nextStack?.y != y1) && nextStack != null);
+	} while ((nextStack?.x !== x1 || nextStack?.y !== y1) && nextStack != null);
 
 	if (nextStack != null) {
 		path.unshift(nextStack);
