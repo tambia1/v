6bcabaa79d395c81d7620067306aed929116c2f4
a <a@a.com>
2025-04-10 00:12:50 +0300

    StyleX

--- app/src/pages/pageHome/components/desktop/apps/testEdit/examples/exampleStyleX/ExampleStyleX.stylex.tsx
+++ app/src/pages/pageHome/components/desktop/apps/testEdit/examples/exampleStyleX/ExampleStyleX.stylex.tsx
@@ -1,22 +1,6 @@
-import { useThemeContext } from "@src/theme/UseThemeContext";
 import * as stylex from "@stylexjs/stylex";
-import { CompiledStyles } from "@stylexjs/stylex/lib/StyleXTypes";
 import { ReactNode } from "react";
-
-export const colors = stylex.defineVars({
-	primary: "color",
-	secondary: "color",
-});
-
-export const lightTheme = stylex.createTheme(colors, {
-	primary: "#000000",
-	secondary: "#ff00ff",
-});
-
-export const darkTheme = stylex.createTheme(colors, {
-	primary: "#ff00ff",
-	secondary: "#000000",
-});
+import { Base, colors } from "./Themes.stylex";
 
 const style = stylex.create({
 	default: {
@@ -24,17 +8,6 @@ const style = stylex.create({
 		backgroundColor: colors.secondary,
 	},
 });
-
-export const Base = ({ children, style, ...props }: { children: ReactNode; style?: CompiledStyles | CompiledStyles[] }) => {
-	const themeContext = useThemeContext();
-	const selectedTheme = { light: lightTheme, dark: darkTheme }[themeContext.theme.themeName];
-
-	return (
-		<div {...stylex.props(selectedTheme, style)} {...props}>
-			{children}
-		</div>
-	);
-};
 
 export const Box = ({ children, ...props }: { children: ReactNode }) => {
 	return (
---
+++ app/src/pages/pageHome/components/desktop/apps/testEdit/examples/exampleStyleX/Themes.stylex.tsx
@@ -0,0 +1,30 @@
+import { useThemeContext } from "@src/theme/UseThemeContext";
+import * as stylex from "@stylexjs/stylex";
+import { CompiledStyles } from "@stylexjs/stylex/lib/StyleXTypes";
+import { ReactNode } from "react";
+
+export const colors = stylex.defineVars({
+	primary: "color",
+	secondary: "color",
+});
+
+export const lightTheme = stylex.createTheme(colors, {
+	primary: "#000000",
+	secondary: "#ff00ff",
+});
+
+export const darkTheme = stylex.createTheme(colors, {
+	primary: "#ff00ff",
+	secondary: "#000000",
+});
+
+export const Base = ({ children, style, ...props }: { children: ReactNode; style?: CompiledStyles | CompiledStyles[] }) => {
+	const themeContext = useThemeContext();
+	const selectedTheme = { light: lightTheme, dark: darkTheme }[themeContext.theme.themeName];
+
+	return (
+		<div {...stylex.props(selectedTheme, style)} {...props}>
+			{children}
+		</div>
+	);
+};
