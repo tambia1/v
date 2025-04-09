55b6d54aefe05c42b6a4779268ae77cd47ded8bc
a <a@a.com>
2025-04-09 23:59:07 +0300

    StyleX

--- app/src/pages/pageHome/components/desktop/apps/testEdit/examples/exampleStyleX/ExampleStyleX.stylex.tsx
+++ app/src/pages/pageHome/components/desktop/apps/testEdit/examples/exampleStyleX/ExampleStyleX.stylex.tsx
@@ -1,5 +1,6 @@
 import { useThemeContext } from "@src/theme/UseThemeContext";
 import * as stylex from "@stylexjs/stylex";
+import { CompiledStyles } from "@stylexjs/stylex/lib/StyleXTypes";
 import { ReactNode } from "react";
 
 export const colors = stylex.defineVars({
@@ -17,20 +18,28 @@ export const darkTheme = stylex.createTheme(colors, {
 	secondary: "#000000",
 });
 
-const stylesThemes = stylex.create({
-	themedBox: {
+const style = stylex.create({
+	default: {
 		color: colors.primary,
 		backgroundColor: colors.secondary,
 	},
 });
 
-export const Box = ({ children, ...props }: { children: ReactNode }) => {
+export const Base = ({ children, style, ...props }: { children: ReactNode; style?: CompiledStyles | CompiledStyles[] }) => {
 	const themeContext = useThemeContext();
 	const selectedTheme = { light: lightTheme, dark: darkTheme }[themeContext.theme.themeName];
 
 	return (
-		<div {...stylex.props(selectedTheme, stylesThemes.themedBox)} {...props}>
+		<div {...stylex.props(selectedTheme, style)} {...props}>
 			{children}
 		</div>
+	);
+};
+
+export const Box = ({ children, ...props }: { children: ReactNode }) => {
+	return (
+		<Base style={[style.default]} {...props}>
+			{children}
+		</Base>
 	);
 };
