12c577a7348eaa46b5a911860a8a1c6b03486bea
a <tambia1@gmail.com>
2026-02-04 23:55:30 +0200

    Commander

--- app/src/pages/pageHome/components/desktop/apps/commander/pages/game/map/Position.ts
+++ app/src/pages/pageHome/components/desktop/apps/commander/pages/game/map/Position.ts
@@ -10,12 +10,17 @@ export class Position {
 	public y: number;
 	public w: number;
 	public h: number;
+	public isPressed: boolean;
+	public isSelected: boolean;
 
 	constructor({ x = 0, y = 0, w = 0, h = 0 }: PositionParams = {}) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
+
+		this.isPressed = false;
+		this.isSelected = false;
 	}
 
 	public setXY(x: number, y: number) {
@@ -52,6 +57,22 @@ export class Position {
 		return this.y + this.h / 2;
 	}
 
+	public setIsPressed(isPressed: boolean) {
+		this.isPressed = isPressed;
+	}
+
+	public getIsPressed() {
+		return this.isPressed;
+	}
+
+	public setIsSelected(isSelected: boolean) {
+		this.isSelected = isSelected;
+	}
+
+	public getIsSelected() {
+		return this.isSelected;
+	}
+
 	public distanceTo(other: Position) {
 		const dx = this.getCenterX() - other.getCenterX();
 		const dy = this.getCenterY() - other.getCenterY();
@@ -62,7 +83,7 @@ export class Position {
 		return x >= this.x && x <= this.x + this.w && y >= this.y && y <= this.y + this.h;
 	}
 
-	public overlaps(other: Position) {
+	public overlaps(other: { x: number; y: number; w: number; h: number }) {
 		return !(this.x + this.w < other.x || other.x + other.w < this.x || this.y + this.h < other.y || other.y + other.h < this.y);
 	}
 }
